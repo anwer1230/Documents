ba2edc74464e67272f8ce08ce28d5316ae3ab389
anwer1230 <anwer@telegram.org>
2026-09-02 15:20:09 +0000

    feat(pwa-notifications): full background web push and gramjs session revocation handling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m1piu3hhcag"
+    "revision": "0.9r744jcpg9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
